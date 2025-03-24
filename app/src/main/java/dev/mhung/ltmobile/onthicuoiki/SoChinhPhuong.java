a3d655c5c042fbde405de6bb3495e43ade18d129
Minh Hùng <hug2k4123@gmail.com>
2025-03-24 19:13:14 +0700

    xong kiem tra so chinh phuong

--- app/src/main/java/dev/mhung/ltmobile/onthicuoiki/SoChinhPhuong.java
+++ app/src/main/java/dev/mhung/ltmobile/onthicuoiki/SoChinhPhuong.java
@@ -1,6 +1,10 @@
 package dev.mhung.ltmobile.onthicuoiki;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SoChinhPhuong extends AppCompatActivity {
+
+    TextView lblSoCPKetQua;
+    EditText txtSoCPNhap;
+    Button btnSoCPKiemTra, btnSoCPQuayLai;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,32 @@ public class SoChinhPhuong extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        addViews();
+        addEvents();
+    }
+
+    private void addViews() {
+        lblSoCPKetQua = (TextView) findViewById(R.id.lblSoCPKetQua);
+        txtSoCPNhap = (EditText) findViewById(R.id.txtSoCPNhap);
+        btnSoCPKiemTra = (Button) findViewById(R.id.btnSoCPKiemTra);
+        btnSoCPQuayLai = (Button) findViewById(R.id.btnSoCPQuayLai);
+    }
+
+    private void addEvents() {
+        btnSoCPKiemTra.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int n = Integer.parseInt(txtSoCPNhap.getText().toString());
+                if (Math.sqrt(n) % 1 == 0) {
+                    lblSoCPKetQua.setText("Kết quả: Số chính phương");
+                } else {
+                    lblSoCPKetQua.setText("Kết quả: Không phải số chính phương");
+                }
+            }
+        });
+
+        btnSoCPQuayLai.setOnClickListener(v ->
+                QuayLai.quayLai(SoChinhPhuong.this, MainActivity.class));
     }
 }
